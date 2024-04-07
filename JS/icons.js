92039ec2502c77ff49f9deb3fb10ea38104680aa
MedzidJasarovic <medzidjasarevic1212@gmail.com>
2024-04-07 17:50:18 +0200

    dodao error evasion tako sto se uneti URL converta u lower case

--- JS/icons.js
+++ JS/icons.js
@@ -2,7 +2,7 @@ const changeSrcBtn = document.getElementById("changeSrcBtn");
 const image = document.querySelector(".ikone img");
 
 changeSrcBtn.addEventListener("click", function () {
-  const noviUrl = document.getElementById("linksajta").value; // Uzima value iz inputa
+  const noviUrl = document.getElementById("linksajta").value.toLowerCase(); // Uzima value iz inputa
   if (noviUrl == "") {
     image.src = "https://icon.horse/icon/wikipedia.org"; //Ukoliko korisnik unese prazno polje vrati se
   } // img.src na placeholder value
