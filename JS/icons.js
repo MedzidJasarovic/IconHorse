c73e5e673087962524020a1f4c2beac5b269912e
MedzidJasarovic <medzidjasarevic1212@gmail.com>
2024-04-07 17:46:37 +0200

    odradio funkcionalnost apia

---
+++ JS/icons.js
@@ -0,0 +1,12 @@
+const changeSrcBtn = document.getElementById("changeSrcBtn");
+const image = document.querySelector(".ikone img");
+
+changeSrcBtn.addEventListener("click", function () {
+  const noviUrl = document.getElementById("linksajta").value; // Uzima value iz inputa
+  if (noviUrl == "") {
+    image.src = "https://icon.horse/icon/wikipedia.org"; //Ukoliko korisnik unese prazno polje vrati se
+  } // img.src na placeholder value
+  else {
+    image.src = "https://icon.horse/icon/" + noviUrl; // Podesavanje img.src na novi value
+  }
+});
