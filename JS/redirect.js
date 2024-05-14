86394ee4759fec1304d7841e4255776f5557e328
MedzidJasarovic <medzidjasarevic1212@gmail.com>
2024-05-14 21:31:09 +0200

    kmt

--- JS/redirect.js
+++ JS/redirect.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   const btns = document.getElementsByClassName("hbtn");
 
-  // Debugging to check if buttons are correctly selected
-  console.log("Buttons found:", btns.length);
+  // Debugging da bi proverili koliko ima elemenata nadjenih sa klasom hbtn , treba da ih bude 4
+  console.log("dugmici nadjeni:", btns.length);
 
-  if (btns.length >= 3) {
-    // Ensure the buttons exist and add event listeners
-
+  if (btns.length >= 4) {
     // Icons button
     btns[0].addEventListener("click", (e) => {
       e.preventDefault();
@@ -52,6 +50,22 @@ document.addEventListener("DOMContentLoaded", (event) => {
         console.log("User logged in. Redirecting to contact.");
         location.href = "../pages/contact.html";
         localStorage.setItem("page", "contact");
+      }
+    });
+
+    // Start now button
+    btns[3].addEventListener("click", (e) => {
+      e.preventDefault();
+      if (
+        !localStorage.getItem("loggedIn") ||
+        localStorage.getItem("loggedIn") === "false"
+      ) {
+        console.log("User not logged in. Redirecting to login.");
+        location.href = "../pages/login.html";
+      } else {
+        console.log("User logged in. Redirecting to icons.");
+        location.href = "../pages/icons.html";
+        localStorage.setItem("page", "icons");
       }
     });
   } else {
